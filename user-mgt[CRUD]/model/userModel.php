c15bddae3a14e8769286863f5c758dfe278f6ea0
HOWLADAR, ROMAN <21-45129-2@student.aiub.edu>
2023-10-23 20:29:56 +0600

    delete and edit user

---
+++ user-mgt[CRUD]/model/userModel.php
@@ -0,0 +1,88 @@
+<?php
+    require_once('db.php');
+
+    function login($username, $password){
+        $con = getConnection();
+        $sql = "select * from users where username='{$username}' and password='{$password}'";
+        $result = mysqli_query($con, $sql);
+        $count = mysqli_num_rows($result);
+
+        if($count == 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function createUser($username,$password,$email){
+        $con = getConnection();
+        $sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
+        
+        if (mysqli_query($con, $sql)) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+    function getUser($id){
+        $con = getConnection();
+        $sql = "select * from users where id = '$id'";
+        $result = mysqli_query($con, $sql);
+        $count = mysqli_num_rows($result);
+        if ($count == 1) {
+            $row = mysqli_fetch_assoc($result);
+            return $row;
+        } else {
+            echo "Invalid User";
+            return false;
+        }
+    }
+
+    function getAllUser(){
+        $con = getConnection();
+        $sql = "select * from users";
+        $result = mysqli_query($con, $sql);
+        $users = [];
+        while($row = mysqli_fetch_assoc($result)){
+            array_push($users, $row);
+        }
+
+        return $users;
+    }
+
+    function updateUser($user){
+
+       
+        $con = getConnection();
+
+
+        $id = $user['id'];
+        $username = $user['username'];
+        $email = $user['email'];
+
+        $sql = "UPDATE users SET username = '$username', email = '$email' WHERE id = $id";
+
+        if (mysqli_query($con, $sql)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function deleteUser($id){
+
+        $con = getConnection();
+        $sql = "DELETE FROM users WHERE id = $id";
+        
+        if (mysqli_query($con, $sql)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+?>
